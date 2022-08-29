ce77fdbea86accce60d8479a5a7bf17bc368b775
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-08-29 11:39:37 +0600

    adeed api

---
+++ js/meals.js
@@ -0,0 +1,10 @@
+const loadMeals = () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals))
+}
+
+const displayMeals = meals => {
+    // console.log(meals)
+}
+loadMeals()
