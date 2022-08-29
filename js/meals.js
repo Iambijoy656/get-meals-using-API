a0d7d340c2bd4225ec2e7b3bf9f69078b1944e86
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-08-29 17:02:16 +0600

    meal details added

--- js/meals.js
+++ js/meals.js
@@ -15,7 +15,7 @@ const displayMeals = meals => {
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
-        <div class="card h-100">
+        <div onclick="loadMealDetails(${meal.idMeal})" class="card h-100">
         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">  
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -37,6 +37,39 @@ const searchFood = () => {
     // console.log('searching', searchText)
     loadMeals(searchText);
     searchField.value = '';
+}
+const loadMealDetails = (idMeal) => {
+    // console.log('get deails of id', idMeal)
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+
+    // console.log(url)
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+
+}
+
+const displayMealDetails = meal => {
+    console.log(meal)
+    const detailContainer = document.getElementById('detail-contaier');
+    detailContainer.innerHTML = ``;
+    window.scrollTo(0, 20)
+    const mealDiv = document.createElement('div');
+    mealDiv.classList.add('card');
+
+    mealDiv.innerHTML = `
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="">${meal.strInstructions.slice(0, 100)}</p>
+                <p class="">Source: ${meal.strYoutube}</p>
+                <a href="#" class="btn btn-primary">Order</a>
+            </div>
+        
+    `
+    detailContainer.appendChild(mealDiv);
+
 }
 
 loadMeals('')
